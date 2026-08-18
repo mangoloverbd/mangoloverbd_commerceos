0ceca521e6d94c811947e0fb435ece51b9a69caa
NK <business.noor@icloud.com>
2026-08-18 21:40:54 +0600

    feat: use bloub animated SVG as Ask Edith sidebar icon

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -94,7 +94,7 @@ export function AppSidebar() {
                 {
                     id: "order-chat",
                     title: "Ask Edith",
-                    icon: <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24" className={iconCls}><path fill="currentColor" d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10H4a2 2 0 0 1-2-2v-8C2 6.477 6.477 2 12 2" opacity=".3" style={{fill: 'var(--fillg)'}}/><path fill="currentColor" d="M15 10H9a1 1 0 0 0-.117 1.993L9 12h6a1 1 0 0 0 .117-1.993zm-3 4H9a1 1 0 1 0 0 2h3a1 1 0 1 0 0-2" style={{fill: 'var(--fillg)'}}/></svg>,
+                    icon: <img src="/bloub-edith.svg" width="15" height="15" alt="Ask Edith" className={iconCls} />,
                     link: "/order-chat",
                 },
                 {
